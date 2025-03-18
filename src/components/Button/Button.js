9fb77e02c98a8af2416416d0d7957ef4dd1437a8
maxjvjohansson <max.jv.johansson@gmail.com>
2025-03-18 16:41:03 +0100

    Setup Next.js project structure with global styles and components

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import "./Button.css";
+
+export default function Button({ text, onClick }) {
+    return (
+        <button className="btn" onClick={onClick}>
+            {text}
+        </button>
+    );
+}
