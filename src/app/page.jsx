37c26c0046e6bbdce9efcb5278b504c5461889ea
maxjvjohansson <max.jv.johansson@gmail.com>
2025-03-19 21:26:20 +0100

    Restructured routing and landingpage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,13 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import Button from "../components/Button/Button";
-import "../styles/pages/home.css";
 
 export default function Home() {
     const router = useRouter();
 
     return (
-        <section className="home">
-            <img src="/assets/images/yrgo.svg" alt="Yrgo Logo" className="logo" />
-            <img src="/assets/images/logo.png" alt="Mingla Logo" className="logo" />
-            <h1>LIA Tinder Möte</h1>
-            <p>Vänligen välj företag eller student</p>
-            <div className="btn-container">
-                <Button text="Företag" onClick={() => router.push("/signup")} />
-                <Button text="Student" onClick={() => router.push("/signup")} />
-            </div>
-        </section>
+        <main>
+
+        </main>
     );
 }
