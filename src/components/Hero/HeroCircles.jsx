95e6bb0677c5537154492649c458a49ecf1b49b7
maxjvjohansson <max.jv.johansson@gmail.com>
2025-03-25 20:09:15 +0100

    Added herocircle avatars, along with basic styling. Also started working on animation

--- src/components/Hero/HeroCircles.jsx
+++ src/components/Hero/HeroCircles.jsx
@@ -2,8 +2,50 @@ import "./HeroCircles.css";
 
 export default function HeroCircles() {
   return (
-    <div className="circle-large">
-      <div className="circle-small"></div>
+    <div className="hero-circles">
+      <div className="circle-large">
+        <div className="orbit orbit-1" style={{ "--angle": "0deg" }}>
+          <figure className="avatar">
+            <img src="/assets/images/Amanda.svg" alt="Amanda – UI/UX Design" />
+            <figcaption>
+              <p>Amanda</p>
+              <p>UI/UX Design</p>
+            </figcaption>
+          </figure>
+        </div>
+
+        <div className="orbit orbit-2">
+          <figure className="avatar" style={{ "--angle": "120deg" }}>
+            <img src="/assets/images/Erik.png" alt="Erik – Web Development" />
+            <figcaption>
+              <p>Erik</p>
+              <p>Web Development</p>
+            </figcaption>
+          </figure>
+        </div>
+
+        <div className="orbit orbit-3" style={{ "--angle": "240deg" }}>
+          <figure className="avatar">
+            <img src="assets/images/Meja.svg" alt="Meja – Motion Design" />
+            <figcaption>
+              <p>Meja</p>
+              <p>Motion Design</p>
+            </figcaption>
+          </figure>
+        </div>
+
+        <div className="circle-small">
+          <div className="orbit orbit-center">
+            <figure className="avatar">
+              <img src="/assets/images/Linnea.png" alt="Linnea – Web Design" />
+              <figcaption>
+                <p>Linnea</p>
+                <p>Web Design</p>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
