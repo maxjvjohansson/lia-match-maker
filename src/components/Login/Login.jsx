b6b7be0e085d479bf3ce7ba6a8a1781fdcfb00de
maxjvjohansson <max.jv.johansson@gmail.com>
2025-03-28 21:30:07 +0100

    Finished mobile layout on login page, also updated button component styling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,39 +1,57 @@
 "use client";
 
-import Button from "../Button/Button";
-import "./Login.css";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import Button from "../Button/Button";
+import ShowIcon from "@/assets/icons/show_password.svg";
+import HideIcon from "@/assets/icons/hide_password.svg";
+import "./Login.css";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <section className="login-container">
-      <form className="login-form">
-        <h1 className="login-heading">Varmt välkomna till</h1>
-        <div className="login-logos">
-          <div className="login-mingla">
-            <Image
-              src="/assets/images/logo.svg"
-              alt="Mingla logo"
-              fill
-              style={{ objectFit: "contain" }}
-            />
-          </div>
-          <div className="login-yrgo">
-            <Image
-              src="/assets/images/yrgo.svg"
-              alt="Yrgo logo"
-              fill
-              style={{ objectFit: "contain" }}
-            />
+    <section className="login-container" aria-labelledby="login-heading">
+      <form
+        className="login-form"
+        method="post"
+        autoComplete="on"
+        aria-describedby="login-description"
+      >
+        <div className="login-heading-wrapper">
+          <h1 id="login-heading" className="login-heading">
+            Varmt välkomna till
+          </h1>
+
+          <div className="login-logos" role="img" aria-label="Mingla by Yrgo">
+            <div className="login-mingla">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="Mingla logo"
+                fill
+                style={{ objectFit: "contain" }}
+              />
+            </div>
+            <div className="login-yrgo">
+              <Image
+                src="/assets/images/yrgo.svg"
+                alt="Yrgo logo"
+                fill
+                style={{ objectFit: "contain" }}
+              />
+            </div>
           </div>
         </div>
 
-        <label htmlFor="email">E-Postadress:</label>
-        <input type="email" id="email" name="email" required />
+        <label htmlFor="email">E-postadress:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          autoComplete="email"
+          required
+        />
 
         <label htmlFor="password">Lösenord:</label>
         <div className="password-wrapper">
@@ -41,24 +59,33 @@ export default function Login() {
             type={showPassword ? "text" : "password"}
             id="password"
             name="password"
+            autoComplete="current-password"
             required
           />
-          <span
+          <button
+            type="button"
             className="toggle-password"
+            aria-label={showPassword ? "Dölj lösenord" : "Visa lösenord"}
             onClick={() => setShowPassword(!showPassword)}
-          ></span>
+          >
+            {showPassword ? (
+              <ShowIcon className="icon" />
+            ) : (
+              <HideIcon className="icon" />
+            )}
+          </button>
         </div>
 
-        <Button text="Logga in" variant="primary" />
-
-        <div className="login-links">
+        <div className="login-links" id="login-description">
           <p>
-            Inget konto? Skapa genom att <a href="">anmäla</a> till eventet.
+            Inget konto? Skapa genom att <Link href="/signup">anmäla dig </Link>
+            till eventet.
           </p>
           <Link href="/reset" className="forgot-link">
             Glömt lösenordet?
           </Link>
         </div>
+        <Button text="Logga in" variant="primary" type="submit" />
       </form>
     </section>
   );
