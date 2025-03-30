698f4c890a38fd88c557077edb157d03de42401f
maxjvjohansson <max.jv.johansson@gmail.com>
2025-03-30 01:11:52 +0100

    Added media queries for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,23 +3,27 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <img
-        src="assets/images/yrgo.svg"
-        alt="Yrgo logo"
-        className="footer-logo"
-      ></img>
-      <p className="footer-slogan">
-        För dig som vill skaffa dig ett yrke. För dig som vill byta yrke och för
-        dig som vill fördjupa dig i ett yrke.
-      </p>
-      <p className="footer-terms">
-        Läs <a href="">användarvillkoren</a>
-      </p>
-      <img
-        src="assets/images/gbg_logo.svg"
-        alt="Gothenburg logo"
-        className="gothenburg-logo"
-      ></img>
+      <div className="footer-left">
+        <img
+          src="assets/images/yrgo.svg"
+          alt="Yrgo logo"
+          className="footer-logo"
+        />
+        <p className="footer-slogan">
+          För dig som vill skaffa dig ett yrke. För dig som vill byta yrke och
+          för dig som vill fördjupa dig i ett yrke.
+        </p>
+        <p className="footer-terms">
+          Läs <a href="">användarvillkoren</a> och <a href="">termerna</a>.
+        </p>
+      </div>
+      <div className="footer-right">
+        <img
+          src="assets/images/gbg_logo.svg"
+          alt="Gothenburg logo"
+          className="gothenburg-logo"
+        />
+      </div>
     </footer>
   );
 }
