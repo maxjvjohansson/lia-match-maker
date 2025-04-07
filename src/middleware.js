680436c814d0e8b463df1660582a9978693ec800
maxjvjohansson <max.jv.johansson@gmail.com>
2025-04-07 21:01:21 +0200

    Added middleware to protect certain routes

---
+++ src/middleware.js
@@ -0,0 +1,9 @@
+import { updateSession } from "@/utils/supabase/middleware";
+
+export async function middleware(request) {
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: ["/dashboard", "/settings", "/event", "/login", "/"],
+};
